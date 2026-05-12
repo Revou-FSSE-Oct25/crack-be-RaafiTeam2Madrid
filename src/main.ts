00ee75e192b25ef0a2a5a83fc95f941ec3945fab
Raafi <raafiyudhatamaaditya@gmail.com>
2026-05-12 20:11:23 +0700

    fix: membuka gerbang CORS secara penuh untuk upload arsip

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
